ceef5838cdcc392c60cf2a5facc2a59262250ebd
robert emerson <roe2pj@virginia.edu>
2024-01-30 18:04:38 -0500

    set up routing

--- app/places-youll-go/src/app/index.tsx
+++ app/places-youll-go/src/app/index.tsx
@@ -1,68 +1,15 @@
+import { RouterProvider } from "react-router-dom";
+
 import "./index.css"
-import { Counter } from "@/features/counter/Counter"
-import { Quotes } from "@/features/quotes/Quotes"
-import logo from '@/logo.png'
-import { Header } from "@/shared/ui/Header"
+import { Header } from "@/shared/ui/Header";
+import { AppRouter } from "./routes";
+
 
 const App = () => {
   return (
     <div className="App">
-      <header className="App-header">
-        <Header />
-        <img src={logo} className="App-logo" alt="logo" />
-        <Counter />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <Quotes />
-        <span>
-          <span>Learn </span>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux.js.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux-toolkit.js.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux Toolkit
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://react-redux.js.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React Redux
-          </a>
-          ,<span> and </span>
-          <a
-            className="App-link"
-            href="https://reselect.js.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Reselect
-          </a>
-        </span>
-      </header>
+      <Header />
+      <RouterProvider router={AppRouter} />
     </div>
   )
 }
---
+++ app/places-youll-go/src/app/routes.tsx
@@ -0,0 +1,15 @@
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+
+import { Counter } from "@/features/counter/Counter"
+import { Quotes } from "@/features/quotes/Quotes"
+
+export const AppRouter = createBrowserRouter([
+  {
+    path: "/",
+    element: <Counter />
+  },
+  {
+    path: "/quotes",
+    element: <Quotes />
+  }
+]);
