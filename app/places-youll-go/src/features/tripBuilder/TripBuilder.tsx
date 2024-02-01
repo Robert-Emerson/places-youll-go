f3d4f2bcfc5812c0af45f8a01a85e3c13007eba1
robert emerson <roe2pj@virginia.edu>
2024-02-01 15:36:00 -0500

    replace component state with redux

--- app/places-youll-go/src/features/tripBuilder/TripBuilder.tsx
+++ app/places-youll-go/src/features/tripBuilder/TripBuilder.tsx
@@ -1,31 +1,28 @@
-import { useState } from "react"
 
 import { List } from "@/features/list/List"
 import { Map } from "@/features/map/Map"
 import tripBuilder from "./TripBuilder.module.css"
+import { useAppDispatch, useAppSelector } from "@/shared/hooks"
 
-enum ViewType { Map = "map", List = "list" }
+import {
+    setMapView,
+    setListView,
+    isListView,
+} from "./tripBuilderSlice"
 
 export const TripBuilder = () => {
-    const [selectedView, setViewType] = useState(import.meta.env.VITE_DEFAULT_VIEW ?? ViewType.Map)
-    function onListSelectClick() {
-        setViewType(ViewType.List);
-    }
 
-    function onMapSelectClick() {
-        setViewType(ViewType.Map);
-    }
+    const dispatch = useAppDispatch()
+    const useList: boolean = useAppSelector(isListView);
 
-    let useList: boolean = selectedView === ViewType.List;
     return (
         <div className={tripBuilder.container}>
             <div className={tripBuilder.viewSelect}>
-                <button onClick={onMapSelectClick} className={!useList ? tripBuilder.active : ""}>Map</button>
+                <button onClick={() => dispatch(setMapView())} className={!useList ? tripBuilder.active : ""}>Map</button>
                 |
-                <button onClick={onListSelectClick} className={useList ? tripBuilder.active : ""}>List</button>
+                <button onClick={() => dispatch(setListView())} className={useList ? tripBuilder.active : ""}>List</button>
             </div>
-            {useList ? <List /> : <Map />}
+            {useList ? <List /> : <Map/>}
         </div>
     )
-
 }
