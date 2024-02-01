4fef71c569f4cf54a9f6471cdaf7d272e6433573
robert emerson <roe2pj@virginia.edu>
2024-01-31 20:04:10 -0500

    add basic footer

---
+++ app/places-youll-go/src/shared/ui-components/Footer.tsx
@@ -0,0 +1,9 @@
+import footer from "./Footer.module.css"
+
+export const Footer = () => {
+    return (
+    <footer className={footer.bottombar}>
+        <a href="https://github.com/Robert-Emerson/places-youll-go">GitHub</a>
+        </footer>
+    )
+}
