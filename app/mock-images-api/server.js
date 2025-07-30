e9eb9abab73417ec5613b7cb67c6dc4b48f82086
emerson <roe2pj@virginia.edu>
2025-07-30 09:51:50 -0400

    add a super fake images API

---
+++ app/mock-images-api/server.js
@@ -0,0 +1,39 @@
+const express = require("express");
+const app = express();
+const port = 3000;
+
+// Mock data
+const imageUrl = `https://live.staticflickr.com/4732/27468187969_32262e8b61_b.jpg`;
+
+app.get("/images", (req, res) => {
+  try {
+    let count = Math.min(req.query.count || 10, 100);
+    let images = new Array(count).fill({
+      coordinates: req.query.coordinates ?? {
+        latitude: 40.71427,
+        longitude: -74.00597,
+      },
+      url: imageUrl,
+      id: null,
+    });
+
+    res.json({
+      images: images.map((image, index) => {
+        return {
+          ...image,
+          coordinates: {
+            latitude: image.coordinates.latitude + 0.25 * index,
+            longitude: image.coordinates.longitude + 0.25 * index,
+          },
+          id: index,
+        };
+      }),
+    });
+  } catch (error) {
+    console.error(error);
+  }
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
